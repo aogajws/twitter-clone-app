015bf5f78afca873760aaebe8b1a53a0e2bad405
strangerxxxx <dendeneden@gmail.com>
2021-09-28 19:45:09 +0900

    create edit page

--- mysite/accounts/forms.py
+++ mysite/accounts/forms.py
@@ -1,11 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User
+from django.forms import ModelForm
 
 
 class LoginForm(AuthenticationForm):
-    # ログインフォーム
-
     def __init__(self, *args, **kw):
         super().__init__(*args, **kw)
         for field in self.fields.values():
@@ -16,3 +14,27 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = User
         fields = UserCreationForm.Meta.fields + ("email",)
+
+
+class UserChangeForm(ModelForm):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+        )
+
+    def __init__(self, username=None, email=None, *args, **kwargs):
+        kwargs.setdefault('label_suffix', '')
+        super().__init__(*args, **kwargs)
+        # ユーザーの更新前情報をフォームに挿入
+        if username:
+            self.fields['username'].widget.attrs['value'] = username
+        if email:
+            self.fields['email'].widget.attrs['value'] = email
+
+    def update(self, user):
+        if user.username != self.cleaned_data['username']:
+            user.username = self.cleaned_data['username']
+        user.email = self.cleaned_data['email']
+        user.save()
--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 app_name = 'accounts'
 urlpatterns = [
-    # path('', views.IndexView.as_view(), name="index"),
     path('login/', views.MyLoginView.as_view(), name="login"),
     path('logout/', views.MyLogoutView.as_view(), name="logout"),
     path('create/', views.UserCreateView.as_view(), name="create"),
+    path('edit/', views.UserChangeView.as_view(), name="edit"),
+    # path('icon/', views.IconChangeView.as_view(), name="icon"),
+    path('password/', auth_views.PasswordChangeView.as_view(
+        success_url=reverse_lazy('accounts:profile')), name="password"),
     path('@<str:username>/', views.user_profile, name='profile'),
     path('remove/@<str:username>/', views.remove, name="remove"),
     path('follow/@<str:username>/', views.follow, name="follow"),
