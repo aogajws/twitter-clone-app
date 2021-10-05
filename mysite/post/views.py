1338b30613127eac487007117feea0755eb0bf8b
aogajws <a_ogawa@jwts.co.jp>
2021-10-05 18:10:39 +0900

    specification change of favorite function

--- mysite/post/views.py
+++ mysite/post/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
-from .models import Post
+from .models import Post, Like
 from . import forms
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -16,8 +16,15 @@ from itertools import chain
 def post_list(request):
     if request.user.is_authenticated:
         user = request.user
+        postlist = Post.objects.filter(author__in=chain(
+            request.user.following.all(), [request.user])).order_by('-created_at')
+        likedlist = []
+        for post in postlist:
+            if post.like_set.filter(user=request.user).exists():
+                likedlist.append(post.pk)
         context = {
-            'post_list': Post.objects.filter(author__in=chain(request.user.following.all(), [request.user])).order_by('-created_at'),
+            'post_list': postlist,
+            'liked_list': likedlist,
             'User': user,
             'description': 'タイムライン'
         }
@@ -41,9 +48,17 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 def favorite(request, pk):
     post = Post.objects.get(pk=pk)
-    post.likes += 1
+    user = request.user
+    like = Like.objects.filter(post=post, user=user)
+    if like.exists():
+        like.delete()
+        post.likes -= 1
+        messages.warning(request, 'いいねを取り消しました。')
+    else:
+        like.create(post=post, user=user)
+        post.likes += 1
+        messages.success(request, 'いいねしました。')
     post.save()
-    messages.success(request, 'いいねしました。')
     return redirect(request.META['HTTP_REFERER'])
 
 
@@ -62,6 +77,11 @@ class SearchPostListView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['User'] = self.request.user
+        likedlist = []
+        for post in context['post_list']:
+            if post.like_set.filter(user=self.request.user).exists():
+                likedlist.append(post.pk)
+        context['liked_list'] = likedlist
         return context
 
     def get_queryset(self):
