f53a1fa9d8b45b1ca0b0de0b2e87fa9f9911dae2
strangerxxxx <dendeneden@gmail.com>
2021-10-06 15:02:21 +0900

    create favorited user page (not completed)

--- mysite/post/urls.py
+++ mysite/post/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = 'post'
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='post_list'),
     path('post_create/', views.PostCreateView.as_view(), name='post_create'),
     path('search/', views.SearchPostListView.as_view(), name='search'),
     path('replies/', views.ReplyPostListView.as_view(), name='replies'),
@@ -12,4 +11,7 @@ urlpatterns = [
     path('reply/<int:pk>/', views.reply_create_view, name='reply'),
     path('favorite/<int:pk>/', views.favorite_view, name='favorite'),
     path('delete/<int:pk>/', views.delete_view, name='delete'),
+    # path('favorited/<int:pk>/',
+    #      views.LikedAccountsListView.as_view(), name='favorited'),
+    path('', views.PostListView.as_view(), name='post_list'),
 ]
